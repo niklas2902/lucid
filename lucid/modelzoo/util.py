ee4ab68bf4eebdaa5d478071e41113ed22fee7ff
Niklas Zimmer <40867286+niklas2902@users.noreply.github.com>
2024-05-15 11:31:22 +0200

    Update util.py

--- lucid/modelzoo/util.py
+++ lucid/modelzoo/util.py
@@ -17,7 +17,7 @@
 
 from __future__ import absolute_import, division, print_function
 
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 import json
 from google.protobuf.message import DecodeError
 import logging
